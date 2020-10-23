ed97da6c515d96324fd0b8e98a3a8cc18ae17eb1
ahnnoah <72554589+ahnnoah@users.noreply.github.com>
2020-10-24 01:45:04 +0900

    Create 마름모.c

---
+++ c언어/2020/도형 그리기/마름모.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+	int n;
+	printf("도형의 크기 입력: ");
+	scanf_s("%d", &n);
+	for (int i = 1; i <= n; i++) {
+		for (int j = 0; j < n - i; j++) {
+			printf("  ");
+		}
+		for (int j = 0; j < 2 * i - 1; j++) {
+			printf("* ");
+		}
+		printf("\n");
+	}
+	for (int i = 1; i <= n; i++) {
+		for (int j = 0; j < i; j++) {
+			printf("  ");
+		}
+		for (int j = 0; j < 2 * (n - i) - 1; j++) {
+			printf("* ");
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
