28c8cf415aaf03e6aa8369e2c85f953a2bd817cd
ahnnoah <72554589+ahnnoah@users.noreply.github.com>
2021-04-07 18:49:17 +0900

    Create 11.cpp

---
+++ C++/2021/4장/11.cpp
@@ -0,0 +1,118 @@
+#include <iostream>
+using namespace std;
+
+class Container {
+	int size;
+public:
+	Container() { size = 10; }
+	void fill();
+	void consume();
+	int getSize();
+};
+void Container::fill() {
+	size = 10;
+}
+void Container::consume() {
+	size -= 1;
+}
+int Container::getSize() {
+	return size;
+}
+
+class CoffeeVendingMachine {
+	Container tong[3];
+	void fill();
+	void selectEspresso();
+	void selectAmericano();
+	void selectsugarCoffee();
+	void show();
+public:
+	void run();
+};
+void CoffeeVendingMachine::fill() {
+	for (int i = 0; i < 3; i++) {
+		tong[i].fill();
+	}
+	show();
+}
+void CoffeeVendingMachine::selectEspresso() {
+	int jg = 0;
+	for (int i = 0; i < 3; i++) {
+		if (tong[i].getSize() == 0) {
+			jg = 1;
+			cout << "원료가 부족합니다." << endl;
+			break;
+		}
+	}
+	if (jg == 0) {
+		tong[0].consume();
+		tong[1].consume();
+		cout << "에스프레소 드세요" << endl;
+	}
+}
+void CoffeeVendingMachine::selectAmericano() {
+	int jg = 0;
+	for (int i = 0; i < 3; i++) {
+		if (tong[i].getSize() == 0) {
+			jg = 1;
+			cout << "원료가 부족합니다." << endl;
+			break;
+		}
+	}
+	if (jg == 0) {
+		tong[0].consume();
+		tong[1].consume();
+		tong[1].consume();
+		cout << "아메리카노 드세요" << endl;
+	}
+}
+void CoffeeVendingMachine::selectsugarCoffee() {
+	int jg = 0;
+	for (int i = 0; i < 3; i++) {
+		if (tong[i].getSize() == 0) {
+			jg = 1;
+			cout << "원료가 부족합니다." << endl;
+			break;
+		}
+	}
+	if (jg == 0) {
+		tong[0].consume();
+		tong[1].consume();
+		tong[1].consume();
+		tong[2].consume();
+		cout << "설탕커피 드세요" << endl;
+	}
+}
+void CoffeeVendingMachine::show() {
+	cout << "커피 " << tong[0].getSize() << ", 물 " << tong[1].getSize() << ", 설탕 " << tong[2].getSize() << endl;
+}
+void CoffeeVendingMachine::run() {
+	cout << "*****커피자판기를 작동합니다. *****" << endl;
+	int num;
+	while (true) {
+		cout << "메뉴를 눌러주세요(1:에스프레소, 2:아메리카노, 3:설탕커피, 4:잔량보기, 5:채우기)>>";
+		cin >> num;
+		switch (num) {
+		case 1:
+			selectEspresso();
+			break;
+		case 2:
+			selectAmericano();
+			break;
+		case 3:
+			selectsugarCoffee();
+			break;
+		case 4:
+			show();
+			break;
+		case 5:
+			fill();
+			break;
+		}
+	}
+}
+
+int main() {
+	CoffeeVendingMachine marchine;
+	marchine.run();
+}
